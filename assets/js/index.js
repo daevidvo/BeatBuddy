13aad0754f3b31bef3792af50782f27f82fabe21
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-11 14:51:09 -0700

    added image api from unsplash to background

--- assets/js/index.js
+++ assets/js/index.js
@@ -98,6 +98,26 @@
 
 
 //BRYAN'S SECTION
+function getUnsplash(){
+  let clientID = "5EBER4yF6LOq05pKyvvFhc3yQkLyidB-RSL4FRW0a3c";
+  let endpoint = "https://api.unsplash.com/photos/random/?query=" + variablemood + "&client_id=" + clientID;
+
+  let imageEl = document.querySelector("#splash-img");
+
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function(jsonData) {
+      imageEl.setAttribute('style', `background-image: url(${jsonData.urls.regular})`)
+    })
+}
+
+ 
+
+  
+  
+  
 
 
 
